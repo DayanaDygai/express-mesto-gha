775f6083a7672153ada35af4e7541baa9008ad2f
Dayana Dygai <dayana.dygai@gmail.com>
2024-01-12 21:56:17 +0400

    13 работа

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 app.use((req, res, next) => {
   req.user = {
