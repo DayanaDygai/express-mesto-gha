40148cad941c4a3fd21269e7f6c0b6392f6fdae8
Dayana Dygai <dayana.dygai@gmail.com>
2024-01-12 22:50:14 +0400

    13 работа

--- models/Card.js
+++ models/Card.js
@@ -1,6 +1,6 @@
-import mongoose, { Schema }  from "mongoose";
+import mongoose,  { Schema }  from "mongoose";
 
-const userSchema = new Schema(
+const cardSchema = new Schema(
   {
     name: {
       type: String,
@@ -8,23 +8,35 @@ const userSchema = new Schema(
       minlength: [2, "минимальная длинна 2 символа"],
       maxlength: [30, "максимальная длинна 30 символов"],
     },
-    about: {
+    link: {
       type: String,
       required: true,
-      minlength: [2, "минимальная длинна 2 символа"],
-      maxlength: [30, "максимальная длинна 30 символов"],
     },
-    avatar: {
-      type: String,
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
       required: true,
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, default: [], ref: "user" }],
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
   {},
 );
 
-export default mongoose.model("user", userSchema);
+export default mongoose.model("card", cardSchema);
 
 // name — имя пользователя, строка от 2 до 30 символов, обязательное поле;
 // about — информация о пользователе, строка от 2 до 30 символов, обязательное поле;
 // avatar — ссылка на аватарку, строка,
 //  обязательное поле.
+
+// timestamp:true
+
+// name — имя карточки, строка от 2 до 30 символов, обязательное поле;
+// link — ссылка на картинку, строка, обязательно поле.
+// owner — ссылка на модель автора карточки, тип ObjectId, обязательное поле;
+// likes — список лайкнувших пост пользователей, массив ObjectId, по умолчанию — пустой массив (поле default);
+// createdAt — дата создания, тип Date, значение по умолчанию Date.now.
