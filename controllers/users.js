bfc12fd3fea43c7866c03934affce941cc044618
Dayana Dygai <dayana.dygai@gmail.com>
2024-01-12 17:21:11 +0400

    13 работа

--- controllers/users.js
+++ controllers/users.js
@@ -89,13 +89,13 @@ export const editInfoUser = async (req, res) => {
 
 export const editAvatarUser = async (req, res) => {
   try {
-    const { avatar } = req.body;
+    const { avatar } = req.body.avatar;
     const user = await User.findByIdAndUpdate(
       req.user._id,
       { avatar },
       { new: "true", runValidators: true },
     ).orFail(() => new Error("NotFoundError"));
-    return res.status(STATUS_OK).send({avatar: user.avatar});
+    return res.status(STATUS_OK).send(user);
   } catch (error) {
     if (error.name === "ValidationError") {
       return res
--- models/Card.js
+++ models/Card.js
@@ -6,8 +6,8 @@ const cardSchema = new Schema(
     name: {
       type: String,
       required: true,
-      minlength: 2,
-      maxlength: 30,
+      minlength: [2, "минимальная длинна 2 символа"],
+      maxlength: [30, "максимальная длинна 30 символов"],
     },
     link: {
       type: String,
