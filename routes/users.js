946a5a48f0fd05575702487ad59276378cb67da8
Dayana Dygai <dayana.dygai@gmail.com>
2024-01-22 04:08:03 +0400

    14 работа

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import {
   getUserById,
   getUsers,
@@ -17,8 +19,19 @@ userRouter.get('/me', getMyProfile);
 
 userRouter.get('/:userId', getUserById);
 
-userRouter.patch('/me', editInfoUser);
+userRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), editInfoUser);
 
-userRouter.patch('/me/avatar', editAvatarUser);
+userRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(
+      /^https?:\/\/(?:www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&`()*+,;=]+#?/,
+    ),
+  }),
+}), editAvatarUser);
 
 export default userRouter;
